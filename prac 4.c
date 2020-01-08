f7fe01eb26856c106ed584377a21a1c2ffda8cfc
MOON0832 <59637978+MOON0832@users.noreply.github.com>
2020-01-08 16:34:09 +0900

    Add files via upload

---
+++ prac 4.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(){
+char c;
+int i,j;
+for(i=1;i<=5;i++){
+    for(c='a';c<='g';c++){
+        printf("%c",c);}
+    printf("\n");
+    for(j=1; j<=7; j++){
+            printf("%d",j);}
+    printf("\n");
+
+}
+
+
+
+}
