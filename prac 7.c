d70c97fc85ec2f69be366d424e3244df59da69b7
MOON0832 <59637978+MOON0832@users.noreply.github.com>
2020-01-08 19:53:41 +0900

    Add files via upload

---
+++ prac 7.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(){
+
+
+int a,b,i,j,z,p,c=0;
+
+printf("블록의 크기:");
+scanf("%d",&a);
+printf("블록의 개수");
+scanf("%d",&b);
+
+for(p=0;p<b;p++){
+for(z=0;z<a;z++){
+for(i=0;i<b;i++){
+        for(j=0;j<a;j++)
+        printf("%d",c);
+        c++;
+
+}
+
+c=0;
+printf("\n");}
+
+printf("\n");
+}
+}
